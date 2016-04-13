5973eacb1ca7f14ca9ad4c03a399ce8818c20f36
Tatsuya Nakano <howdy@Tatsuyas-MacBook.local>
2016-04-13 21:58:14 +0900

    Step 2 Mochaを導入

--- test/calc-util-spec.js
+++ test/calc-util-spec.js
@@ -1,10 +1,17 @@
 'use strict';
 
-console.info('add関数のテスト');
-console.info('1+2は3である');
+describe('add関数のテスト', function() {
+    it('1+2は3である', function() {
+        if (add(1, 2) === 3) {
+        } else {
+            throw new Error('失敗');
+        }
+    });
+    it('1+2は4である', function() {
+        if (add(1, 2) === 4) {
+        } else {
+            throw new Error('失敗');
+        }
+    });
+});
 
-if (add(1, 2) === 3) {
-    console.info('成功')
-} else {
-    console.error('失敗')
-}
