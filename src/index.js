e02104e21fd1dda9def6df4ef31a51561306cb0a
Tatsuya Nakano <howdy@Tatsuyas-MacBook.local>
2016-04-14 21:06:55 +0900

    Step 4 webpackを導入

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 'use strict';
 
-document.getElementById('calc').addEventListener('click', function clickCalc() {
+var calcUtil = require('./calc-util.js');
+
+document.getElementById('calc').addEventListener('click', function () {
     var value1 = Number(document.getElementById('value1').value);
     var value2 = Number(document.getElementById('value2').value);
     
-    var result = add(value1, value2);
+    var result = calcUtil.add(value1, value2);
     document.getElementById('result').innerText = result;
 });
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+    entry: {
+        app: './src/index.js'
+    },
+    output: {
+        path: './dist',
+        filename: '[name].bundle.js'
+    }
+};
