ec73eb2b32e4a5966978212fa1328e3be1d693c4
Tatsuya Nakano <howdy@Tatsuyas-MacBook.local>
2016-04-17 21:03:15 +0900

    Step 5 assertを導入

--- test/calc-util-spec.js
+++ test/calc-util-spec.js
@@ -1,19 +1,13 @@
 'use strict';
 
+var assert = require('assert');
 var calcUtil = require('../src/calc-util.js');
 
 describe('add関数のテスト', function() {
     it('1+2は3である', function() {
-        if (calcUtil.add(1, 2) === 3) {
-        } else {
-            throw new Error('失敗');
-        }
+        assert(calcUtil.add(1, 2) === 3);
     });
     it('1+2は4である', function() {
-        if (calcUtil.add(1, 2) === 4) {
-        } else {
-            throw new Error('失敗');
-        }
+        assert(calcUtil.add(1, 2) === 4);
     });
 });
-
